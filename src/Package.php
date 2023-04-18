449e8a2e932b4c4f3c0f41dfacfed2137d210b55
wooiliang <chuawooiliang@gmail.com>
2023-04-18 08:24:42 +0800

    WIP

--- src/Package.php
+++ src/Package.php
@@ -37,14 +37,14 @@ class Package
 
     public ?string $publishableProviderName = null;
 
-    public function name(string $name): static
+    public function name(string $name)
     {
         $this->name = $name;
 
         return $this;
     }
 
-    public function hasConfigFile($configFileName = null): static
+    public function hasConfigFile($configFileName = null)
     {
         $configFileName = $configFileName ?? $this->shortName();
 
@@ -57,14 +57,14 @@ class Package
         return $this;
     }
 
-    public function publishesServiceProvider(string $providerName): static
+    public function publishesServiceProvider(string $providerName)
     {
         $this->publishableProviderName = $providerName;
 
         return $this;
     }
 
-    public function hasInstallCommand($callable): static
+    public function hasInstallCommand($callable)
     {
         $installCommand = new InstallCommand($this);
 
@@ -80,7 +80,7 @@ class Package
         return Str::after($this->name, 'laravel-');
     }
 
-    public function hasViews(string $namespace = null): static
+    public function hasViews(string $namespace = null)
     {
         $this->hasViews = true;
 
@@ -89,14 +89,14 @@ class Package
         return $this;
     }
 
-    public function hasViewComponent(string $prefix, string $viewComponentName): static
+    public function hasViewComponent(string $prefix, string $viewComponentName)
     {
         $this->viewComponents[$viewComponentName] = $prefix;
 
         return $this;
     }
 
-    public function hasViewComponents(string $prefix,  ...$viewComponentNames): static
+    public function hasViewComponents(string $prefix,  ...$viewComponentNames)
     {
         foreach ($viewComponentNames as $componentName) {
             $this->viewComponents[$componentName] = $prefix;
@@ -105,14 +105,14 @@ class Package
         return $this;
     }
 
-    public function sharesDataWithAllViews(string $name, $value): static
+    public function sharesDataWithAllViews(string $name, $value)
     {
         $this->sharedViewData[$name] = $value;
 
         return $this;
     }
 
-    public function hasViewComposer($view, $viewComposer): static
+    public function hasViewComposer($view, $viewComposer)
     {
         if (! is_array($view)) {
             $view = [$view];
@@ -125,35 +125,35 @@ class Package
         return $this;
     }
 
-    public function hasTranslations(): static
+    public function hasTranslations()
     {
         $this->hasTranslations = true;
 
         return $this;
     }
 
-    public function hasAssets(): static
+    public function hasAssets()
     {
         $this->hasAssets = true;
 
         return $this;
     }
 
-    public function runsMigrations(bool $runsMigrations = true): static
+    public function runsMigrations(bool $runsMigrations = true)
     {
         $this->runsMigrations = $runsMigrations;
 
         return $this;
     }
 
-    public function hasMigration(string $migrationFileName): static
+    public function hasMigration(string $migrationFileName)
     {
         $this->migrationFileNames[] = $migrationFileName;
 
         return $this;
     }
 
-    public function hasMigrations(...$migrationFileNames): static
+    public function hasMigrations(...$migrationFileNames)
     {
         $this->migrationFileNames = array_merge(
             $this->migrationFileNames,
@@ -163,28 +163,28 @@ class Package
         return $this;
     }
 
-    public function hasCommand(string $commandClassName): static
+    public function hasCommand(string $commandClassName)
     {
         $this->commands[] = $commandClassName;
 
         return $this;
     }
 
-    public function hasCommands(...$commandClassNames): static
+    public function hasCommands(...$commandClassNames)
     {
         $this->commands = array_merge($this->commands, collect($commandClassNames)->flatten()->toArray());
 
         return $this;
     }
 
-    public function hasRoute(string $routeFileName): static
+    public function hasRoute(string $routeFileName)
     {
         $this->routeFileNames[] = $routeFileName;
 
         return $this;
     }
 
-    public function hasRoutes(...$routeFileNames): static
+    public function hasRoutes(...$routeFileNames)
     {
         $this->routeFileNames = array_merge($this->routeFileNames, collect($routeFileNames)->flatten()->toArray());
 
@@ -205,7 +205,7 @@ class Package
         return $this->viewNamespace ?? $this->shortName();
     }
 
-    public function setBasePath(string $path): static
+    public function setBasePath(string $path)
     {
         $this->basePath = $path;
 
